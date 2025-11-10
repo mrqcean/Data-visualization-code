daa5f0b48cc06e3d490e249aa3a47310e90bd8a4
zenrough <12938899-Zenrough@users.noreply.gitlab.com>
2025-11-10 11:29:24 +0100

    made preprocessing

--- main.R
+++ main.R
@@ -10,9 +10,34 @@ install.packages("readxl")
 library("readxl")
 
 ###### GWP visualization 
-data <- read_excel("./datasets/IPCC_AR4-AR6_GWPs.xlsx")
+data <- read_excel("./datasets/IPCC_AR4-AR6_GWPs.xlsx",sheet = "Main")
 source("./scripts/gwp-cumulative-over-time.R")
 gwplinegraph(data)
+
+##### main dataset
+# save as different main_data <- read_excel("")
+install.packages("shinydashboard")
+library(shinydashboard)
+
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
+
+
+# emissions by industry
+emissions_by_industry <- read_excel("./datasets/EDGAR-FOOD_v61_AP.xlsx",sheet = "Suppl. Table 3-Emi by stage")
+# "non-dr" congo, elsalvador, singapore og danmark
+# these are row  274, 1191,1167 ,361
+countries <- slice(emissions_by_industry, 274,1191,1167,361)
 
 ##### Importing scripts
 # source does not work recursively, else we have to define a custom 
--- scripts/gwp-cumulative-over-time.R
+++ scripts/gwp-cumulative-over-time.R
@@ -1,4 +1,6 @@
 # this defines the function the create the graph which is imported in main.r in root dir
+
 gwplinegraph <- function(dataset) {
+# we only use the data from ar6 as that is the newest iteration of the gwp estimations 
   
 }
